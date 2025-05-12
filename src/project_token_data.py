8ef8ce0802d30489a4bac1b700ef44d53f8e72f3
Rishik00 <96735720+Rishik00@users.noreply.github.com>
2025-05-12 10:19:46 +0530

    added config and modified main

--- src/project_token_data.py
+++ src/project_token_data.py
@@ -13,15 +13,27 @@ def main(
     translator_config = load_translator_config(config_name)
     alignment_config_yaml = load_aligner_config(config_name)
 
-    source_lang_sentence = [
-        "Back in school, I walked to the library every afternoon.",
-        "When we were kids, we played cricket in the streets until sunset.",
-        "Every summer, I visited my grandparents in the countryside.",
-        "I used to read comic books under the mango tree after lunch.",
+    source = [
+        {
+            "sentence": "Back in school, I walked to the library every afternoon.",
+            "ner_tags": ["O", "O", "O", "O", "O", "O", "O", "O", "B-LOC", "O", "O", "O"]
+        },
+        {
+            "sentence": "When we were kids, we played cricket in the streets until sunset.",
+            "ner_tags": ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "B-LOC", "O", "O", "O"]
+        },
+        {
+            "sentence": "Every summer, I visited my grandparents in the countryside.",
+            "ner_tags": ["O", "B-TIME", "O", "O", "O", "O", "O", "O", "O", "B-LOC", "O"]
+        },
+        {
+            "sentence": "I used to read comic books under the mango tree after lunch.",
+            "ner_tags": ["O", "O", "O", "O", "O", "O", "O", "O", "B-LOC", "I-LOC", "O", "B-TIME", "O"]
+        }
     ]
 
     translator_config = TranslatorConfig(
-        batch_size=translator_config.get('batch_size', 4),
+        batch_size=int(translator_config.get('batch_size', 4)),
         model_checkpoint_dir=translator_config.get('model_checkpoint_dir', ''),
         q_config=translator_config.get('q_config', '')
     )
@@ -29,7 +41,7 @@ def main(
     aligner_config = AlignerConfig(
         model=alignment_config_yaml.get('model', 'bert-base-multilingual-cased'),
         output_file=alignment_config_yaml.get('output_file','content/dummty.txt'),
-        align_layer=alignment_config_yaml.get('align_layer', 8),
+        align_layer=int(alignment_config_yaml.get('align_layer', 8)),
         threshold=float(alignment_config_yaml.get('threshold', 0.3)),
         probs_layer=alignment_config_yaml.get('probs_layer', 'softmax'),
     )
@@ -39,17 +51,24 @@ def main(
     aligner = BaseAlignmentModel(config=aligner_config)
 
     # Translate a sentence
-    translations = translator_model.batch_translate(source_lang_sentence)
-    print(f"Translation: {translations}")
+    for i in range(len(source)):
+        source_lang_sentence, ner_tags = [source['sentence']], source['ner_tags']
+        print(source_lang_sentence, ner_tags)
 
-    print(source_lang_sentence, translations)
-    for i in range(len(translations)):
-
-        source_sentence, target_sentence = source_lang_sentence[i], translations[i]
-        print("Doing something: ", source_sentence, target_sentence)
+        translations = translator_model.batch_translate(source_lang_sentence)
+        print(source_lang_sentence, translations)
         
-        aligned_words = aligner.get_alignments(source_sentence, target_sentence)
-        print(aligned_words)
+        for i in range(len(translations)):
+
+            source_sentence, target_sentence = source_lang_sentence[i], translations[i]
+            print("Doing something: ", source_sentence, target_sentence)
+            
+            aligned_words = aligner.get_alignments(source_sentence, target_sentence)
+            print(aligned_words)
+
+            print("Aligning translated words with their NER tags")
+
+
 
 if __name__ == "__main__":
     typer.run(main)
